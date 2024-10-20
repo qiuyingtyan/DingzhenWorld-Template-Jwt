4efa4e4567a76842fcaf2478bb31ca4ff19d738b
riko3 <3396149374@qq.com>
2024-10-20 12:51:19 +0800

    配置SpringSecurity基本内容

--- my-project-backend/src/main/java/com/example/MyProjectBackendApplication.java
+++ my-project-backend/src/main/java/com/example/MyProjectBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.myprojectbackend;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
